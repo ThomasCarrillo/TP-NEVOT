c704ac1fadb880e3b28b80ac024a84e1b6536b7a
Tehnecens <blasquezdylan0@gmail.com>
2018-10-31 17:04:45 +0100

    Update php/mail.php

---
+++ php/mail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>FormArt-In</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Anton|Black+Han+Sans|Righteous" rel="stylesheet">
+</head>
+<body>
+	<?php include 'header.php' ?>
+	<form action="mail.php" method="post">
+
+	</form>
+	<?php include 'footer.php' ?>		
+</body>
+</html>
