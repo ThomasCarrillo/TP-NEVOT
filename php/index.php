465b4df708b8b6cdd758e9888c3b18bef572c765
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2018-11-05 16:29:19 +0100

    Update php/index.php

--- php/index.php
+++ php/index.php
@@ -9,16 +9,6 @@
 	<link href="https://fonts.googleapis.com/css?family=Anton|Black+Han+Sans|Righteous" rel="stylesheet">
 </head>
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1be90aba8adfca93b9e69684d99a2dd04a6fb26c
-=======
->>>>>>> 1be90aba8adfca93b9e69684d99a2dd04a6fb26c
->>>>>>> 9a44b539b56f01628623617ebaa77bf574c5b849
 	<?php include 'header.php' ?>
 	<div class="pdigme1202_12">
 		<div class="pdigme1202_8">
@@ -28,19 +18,11 @@
 				    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Bonum incolumis acies: misera caecitas. Idem iste, inquam, de voluptate quid sentit? Aufert enim sensus actionemque tollit omnem. Erat enim Polemonis. Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Hoc enim constituto in philosophia constituta sunt omnia. Duo Reges: constructio interrete. Dicet pro me ipsa virtus nec dubitabit isti vestro beato M. Sed ut iis bonis erigimur, quae expectamus, sic laetamur iis, quae recordamur. Et quidem illud ipsum non nimium probo et tantum patior, philosophum loqui de cupiditatibus finiendis. At multis malis affectus. Non igitur bene. Gloriosa ostentatio in constituendo summo bono. Maximus dolor, inquit, brevis est. Quid nunc honeste dicit? Haec quo modo conveniant, non sane intellego. Haec dicuntur inconstantissime. At enim hic etiam dolore. Nunc haec primum fortasse audientis servire debemus. Nummus in Croesi divitiis obscuratur, pars Quae autem natura suae primae institutionis oblita est? Vitiosum est enim in dividendo partem in genere numerare. Quae in controversiam veniunt, de iis, si placet, disseramus. Nobis aliter videtur, recte secusne, postea; Quid, de quo nulla dissensio est?
 				</section>
 			</div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 9a44b539b56f01628623617ebaa77bf574c5b849
 	</div>
 </div>
 <span class="one"></span>
 <video class="video" src="../ressources/video/Dramatic_Chipmunk.mp4" type="video/mp3" controls></video>
-<<<<<<< HEAD
 <div class="formulaire">
-=======
-=======
 			<span></span>
 			<div class="video">
 				<video src="../ressources/video/Dramatic Chipmunk.mp3" autoplay poster="posterimage.jpg">
@@ -49,8 +31,6 @@
 			</div>
 		</div>
 	</div>
->>>>>>> 1be90aba8adfca93b9e69684d99a2dd04a6fb26c
-=======
 			<span></span>
 			<div class="video">
 				<video src="../ressources/video/Dramatic Chipmunk.mp3" autoplay poster="posterimage.jpg">
@@ -59,8 +39,6 @@
 			</div>
 		</div>
 	</div>
->>>>>>> 1be90aba8adfca93b9e69684d99a2dd04a6fb26c
->>>>>>> 9a44b539b56f01628623617ebaa77bf574c5b849
 	<form action="mail.php" method="post">
 		<input type="text" name="prenom">
 		<input type="radio" name="sexe" value="homme" id="homme"> <label for="oui">homme</label>
@@ -76,11 +54,8 @@
 				<option value="allemagne">Allemagne</option>
 		</select>
 		<input type="checkbox" name="condition">
-<<<<<<< HEAD
 		<input type="submit" value="Valider" />
-=======
 	  <input type="submit" value="Valider" />
->>>>>>> 9a44b539b56f01628623617ebaa77bf574c5b849
 	</form>
 </div>
 
