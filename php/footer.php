2a6a7027709eeed49623f3cb9a85dfaa50dbec7e
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2018-11-08 14:31:29 +0100

    Update php/footer.php

--- php/footer.php
+++ php/footer.php
@@ -1,6 +1,8 @@
 <footer class="col-s-12">
 	<h3>Contact</h3>
 	<div class="social">
-		<img src="../ressources/images/twitter.png" alt="twitter" title="Twitter">
+		<img class="twitter" src="../ressources/images/twitter.png" alt="twitter" title="Twitter">
+		<img class="facebook" src="../ressources/images/facebook.png" alt="twitter" title="Facebook">
+		<img class="linkedin" src="../ressources/images/linkedin.png" alt="twitter" title="LinkedIn">
 	</div>
 </footer>
