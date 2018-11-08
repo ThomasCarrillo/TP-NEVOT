38b1b89aa58793fdee9e48e4e53c87ec5d9b4b11
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2018-11-08 17:12:13 +0100

    Update php/footer.php

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,11 @@
 <footer class="col-s-12">
-	<h3>Contact</h3>
 	<div class="social">
-		<img class="twitter" src="../ressources/images/twitter.png" alt="twitter" title="Twitter">
+		<!--<img class="twitter" src="../ressources/images/twitter.png" alt="twitter" title="Twitter">
 		<img class="facebook" src="../ressources/images/facebook.png" alt="twitter" title="Facebook">
-		<img class="linkedin" src="../ressources/images/linkedin.png" alt="twitter" title="LinkedIn">
+		<img class="linkedin" src="../ressources/images/linkedin.png" alt="twitter" title="LinkedIn">-->
+
+		<a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a>
+		<a href="https://twitter.com/?lang=fr" target="_blank"><i class="fa fa-twitter"></i></a>
+		<a href="https://www.linkedin.com/feed/" target="_blank"><i class="fa fa-linkedin"></i></a>
 	</div>
 </footer>
